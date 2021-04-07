3be5eda701af026ae1748b372511630d991204e7
utkusarioglu <utkusarioglu@hotmail.com>
2021-04-07 07:16:09 +0000

    Feat: add build.sh
    * This script adds the ability to create a theme build without dealing
    with npm scripts and manual cleaning after the build is complete

---
+++ build.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+source .env
+# Remove old build
+rm -rf build
+
+# handle build
+cd theme/react-src 
+yarn build
+
+cd ../..
+
+# move the build to the root/build dir
+# ! note that theme/theme may cause some errors, it's not clear whether
+# ! this name is dependend on css theme name
+mkdir -p build/$THEME_NAME
+mv theme/theme/* build/$THEME_NAME
+
+# Remove build artifacts. The same piece of code is also used in theme_clean.sh
+cd theme
+ls --hide=react-src | xargs -d '\n' rm -rf
+
+cat << EOF 
+
+Build complete. You can find your theme in "build/$THEME_NAME".
+
+The build operation removes some files that are necessary for development. 
+To mitigate this, you can restart your devcontainer, then you can continue to
+work normally.
+
+EOF
