f553c2e96a05f4fc312990ba9a3bca25efd899bc
utkusarioglu <utkusarioglu@hotmail.com>
2021-04-07 10:57:52 +0000

    Feat: add filename specification to docker_prune
    * docker_prune now allows both skipping backup creation as well as
    setting a custom filename for the backup that will be created. It
    redirects the -f, --filename params to db_backup.sh
    
    * docker stdout is now being sent to /dev/null while stderr still
    staying on console.
    
    * Long errors have been refactored as separate functions

--- docker_prune.sh
+++ docker_prune.sh
@@ -1,34 +1,77 @@
 #!/bin/bash
 
-source .env
+function theme_name_not_defined_error {
+cat >&2 << EOF
 
-if ! test -f ".env"; then 
-  cat >&2 << EOF
 Operation failed. This script requires the following properties from the .env file:
 
 THEME_NAME: The name for the theme that you are developing
 
 Please make sure that an .env file exists and the listed properties are set, 
 and then rerun this script.
+
 EOF
-exit 1;
+}
+
+source .env
+
+# Vars
+no_backup=FALSE
+backup_file=""
+
+# Parses input params to the relevant variables listed above
+function handle_input {
+  while test $# -gt 0; do
+    case "$1" in
+      -f|--filename)
+        shift
+        backup_file=$1
+        shift
+        ;;&
+
+      -n|--no-backup) 
+        no_backup=TRUE
+        shift
+        ;;&
+
+      *)
+        # TODO find a way to avoid this empty string check
+        if [ ! -z $1 ]; then
+          echo "-${1}-"
+          invalid_flag_error
+          exit 1;
+        fi
+      ;;
+    esac
+  done 
+}
+handle_input $@
+
+
+if ! test -f ".env"; then 
+  theme_name_not_defined_error
+  exit 1;
 fi
 
 echo "Running pruning for ${THEME_NAME}..."
 
-if [[ "$@" =~ "--no-backup" ]]; then
+if [[ $no_backup == TRUE ]]; then
   echo "Skipping db backup creation upon user command"
 else
-  bash ./db_backup.sh
+  if [ -z $backup_file ]; then
+    bash ./db_backup.sh
+  else 
+    bash ./db_backup.sh -f $backup_file
+  fi
 fi
 
 echo "Cleaning containers, volumes, and networks"
 
 for command in 'stop' 'rm';
 do
-  docker container $command "${THEME_NAME}__wp" "${THEME_NAME}__db"  
+  docker container $command "${THEME_NAME}__wp" "${THEME_NAME}__db" 1> /dev/null
 done;  
 
 repo_name="$(basename "$PWD")"
-docker volume rm "${repo_name}_db"
-docker network rm "${repo_name}_default"
+docker volume rm "${repo_name}_db" 1> /dev/null
+docker network rm "${repo_name}_default" 1> /dev/null
