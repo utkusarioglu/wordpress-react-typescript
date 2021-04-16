9b27a97f86ff3fef93c77df86ca29e62795ca84c
utkusarioglu <utkusarioglu@hotmail.com>
2021-04-16 08:42:55 +0000

    Feat: implement separare sh file for config
    * There are unrelated script files that define and use related config
    values. This is error prone. This commit creates a new sh file that
    gathers all of these values in one file.

---
+++ scripts/host/vars.sh
@@ -0,0 +1,7 @@
+# This file contains variables relevant for more than one script file
+# Be weary of altering these values without learning where they occur
+# Not all (ie docker-compose) use variables to set these
+
+HOST_BACKUPS_DIR=backups/sql
+CONTAINER_BACKUPS_DIR=backups/sql
+THEME_DIR=theme/react-src
