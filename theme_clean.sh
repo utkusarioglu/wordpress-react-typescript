4dcf3ea4720aa6152697043e8280af8caa5e9d90
utkusarioglu <utkusarioglu@hotmail.com>
2021-04-19 09:23:56 +0000

    Chore: add spacing to theme_clean message

--- theme_clean.sh
+++ theme_clean.sh
@@ -8,6 +8,8 @@ rm -rf node_modules
 rm -rf build
 
 cat << EOF
+
 All non-essential files have been removed from the theme directory.
 You can run bootstrap.sh to restore dependencies and other runtime files.
+
 EOF
