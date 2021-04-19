c95086cb39fe7b145875e4f5ec28db5e063e73df
utkusarioglu <utkusarioglu@hotmail.com>
2021-04-19 09:42:53 +0000

    Update: build dir set mechanism
    * Before, the shell script was carrying the build folder up to the
    project root. Now the user.prod.json setting handles where the build
    is created.
    
    * theme_build.sh has been updated according to the change in
    user.prod.json.
    
    * An unneeded file in build is removed by the shell script upon build
     creation.

--- theme_build.sh
+++ theme_build.sh
@@ -11,26 +11,15 @@ rm -rf build
 echo "Building theme ${THEME_NAME}"
 cd theme/react-src 
 yarn build
-
-
-# move the build to the root/build dir
-# ! note that theme/theme may cause some errors, it's not clear whether
-# ! this name is dependent on css theme name
 cd ../..
-mkdir -p build/$THEME_NAME
-mv theme/theme/* build/$THEME_NAME
 
-# Remove build artifacts. The same piece of code is also used in theme_clean.sh
+# Remove irrelevant files
 echo "Removing clutter..."
-cd theme
-ls --hide=react-src | xargs -d '\n' rm -rf
+cd build
+rm ./!READY_TO_DEPLOY!.txt
 
 cat << EOF 
 
-Build complete. You can find your theme in "build/$THEME_NAME".
-
-The build operation removes some files that are necessary for development. 
-To mitigate this, you can restart your devcontainer, then you can continue to
-work normally.
+Build complete. You can find your theme in "build".
 
 EOF
